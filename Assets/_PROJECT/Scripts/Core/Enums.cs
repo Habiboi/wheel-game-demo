de8affad837d4ffd1ff053d9385ad7fd96c7acee
Habiboi <habip.sana.76@gmail.com>
2026-01-06 15:55:45 +0300

    Enums

---
+++ Assets/_PROJECT/Scripts/Core/Enums.cs
@@ -0,0 +1,31 @@
+namespace Project.Core.Enums
+{
+    public enum WheelSliceType
+    {
+        Reward,
+        Bomb
+    }
+
+    public enum ZoneType
+    {
+        Normal,
+        Safe,
+        Super
+    }
+
+    public enum GameState
+    {
+        Idle,
+        Spinning,
+        Reward,
+        Bomb,
+        GameOver
+    }
+
+    public enum SpinType
+    {
+        Normal,
+        Silver,
+        Gold
+    }
+}
